0ea629fdf6e9e9084b539f1302422db7076a32fa
Fensley <zagalufe@gmail.com>
2024-03-29 12:13:57 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,6 @@
+// Write your JavaScript code here
+const btn = document.getElementById("btn");
+
+btn.addEventListener("click", () => {
+    alert("Please check your email to get your christmas gift ")
+})
